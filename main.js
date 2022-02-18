3c2d68073c5a9930590c8987fc17231bd39ee7a3
DarkRedCherry <91740376+DarkRedCherry@users.noreply.github.com>
2022-02-18 14:12:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var first_name = "Rocky";
+console.log(first_name);
+
+var last_name = "Singh";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var name_one = "I am Aashika Mandalika";
+console.log(name_one);
+var replace_name = name_one.replace("Dahl","Tobi");
+console.log(replace_name);
+
+var capital_name = "ROCKY";
+
+var making_lower_case = capital_name.toLowerCase();
+var small_name = "Aashika";
+
+var making_lower_case = small_name.toUpperCase();
+
+var square_root = Math.sqrt(16);
+
+var maximum_number = Math.max(67, 89);
+
+var mnimum_number = Math.min(78, 45);
+
+var power_eg_1 = Math.pow(3, 2);
+
+var power_eg_2 = Math.pow(6, 2);
